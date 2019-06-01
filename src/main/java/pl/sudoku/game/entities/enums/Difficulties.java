406812223ecf2fe3071a747739362210e9ce9cc6
Pplociennik <przemx.84@gmail.com>
2019-06-01 16:42:08 +0200

    Added difficulties enum

--- src/main/java/pl/sudoku/game/entities/enums/Difficulties.java
+++ src/main/java/pl/sudoku/game/entities/enums/Difficulties.java
@@ -1,5 +1,15 @@
 package pl.sudoku.game.entities.enums;
 
 public enum Difficulties {
-    EASY, MEDIUM, HARD
+    EASY(20), MEDIUM(30), HARD(40);
+
+    private final int diffCode;
+
+    Difficulties(int value) {
+        this.diffCode = value;
+    }
+
+    public int getDiffCode() {
+        return this.diffCode;
+    }
 }
