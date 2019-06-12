77409c18b3c98ad6ced74ee4797ecbea5fff81db
Pplociennik <przemx.84@gmail.com>
2019-06-12 02:39:13 +0200

    .

--- src/main/java/pl/sudoku/game/database/entities/Result.java
+++ src/main/java/pl/sudoku/game/database/entities/Result.java
@@ -1,10 +1,12 @@
 package pl.sudoku.game.database.entities;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Result {
 
     @Id
