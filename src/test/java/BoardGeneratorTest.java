998b0a85084a043d96525b06e3a76d34c354ef21
KamilFig <figurka1234@gmail.com>
2019-06-13 09:27:39 +0200

    Adding levelTest

--- src/test/java/BoardGeneratorTest.java
+++ src/test/java/BoardGeneratorTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
+import pl.sudoku.game.entities.enums.Difficulties;
 import pl.sudoku.game.processing.BoardGenerator;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 
 public class BoardGeneratorTest {
@@ -14,4 +16,56 @@ public class BoardGeneratorTest {
 
         assertNotNull(result);
     }
+    @Test
+    public void shouldRemove20DigitsOnEasyMode(){
+        BoardGenerator generator = new BoardGenerator();
+        generator.prepare(9, Difficulties.EASY.getDiffCode());
+        generator.fillValues();
+        generator.removeKDigits();
+        int [][] result = generator.getMat();
+        int count = 0;
+        for(int i= 0 ; i < 9 ; i++)
+            for(int j= 0 ; j < 9; j++)
+                if(result[i][j] == 0){
+                    count++;
+                }
+
+        assertEquals(count,20);
+
+    }
+    @Test
+    public void shouldRemove30DigitsOnMediumMode(){
+        BoardGenerator generator = new BoardGenerator();
+        generator.prepare(9, Difficulties.MEDIUM.getDiffCode());
+        generator.fillValues();
+        generator.removeKDigits();
+        int [][] result = generator.getMat();
+        int count = 0;
+        for(int i= 0 ; i < 9 ; i++)
+            for(int j= 0 ; j < 9; j++)
+                if(result[i][j] == 0){
+                    count++;
+                }
+
+        assertEquals(count,30);
+
+    }
+    @Test
+    public void shouldRemove40DigitsOnMediumMode(){
+        BoardGenerator generator = new BoardGenerator();
+        generator.prepare(9, Difficulties.HARD.getDiffCode());
+        generator.fillValues();
+        generator.removeKDigits();
+        int [][] result = generator.getMat();
+        int count = 0;
+        for(int i= 0 ; i < 9 ; i++)
+            for(int j= 0 ; j < 9; j++)
+                if(result[i][j] == 0){
+                    count++;
+                }
+
+        assertEquals(count,40);
+
+    }
+
 }
